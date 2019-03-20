a832ca246683367bd1372eae2e47a8f13ab0de85
User <User@192.168.178.20>
2019-03-20 12:06:17 +0100

    Aggiunta metodi Libretto.

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -1,5 +1,20 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+import java.util.*;
+
 public class Libretto {
+	private List<Voto> voti; 
+	
+	public Libretto() {
+		this.voti= new ArrayList<Voto>();
+	}
+	
+	public void add(int voto, String corso, LocalDate data) {
+		
+	}
+	public void add(Voto v) {
+		
+	}
 
 }
