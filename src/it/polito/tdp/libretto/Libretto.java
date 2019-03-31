8c318aefa2bb93fd17c9f9efca33b268b3fc2b87
User <User@192.168.178.20>
2019-03-31 18:48:16 +0200

    punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -50,6 +50,22 @@ public class Libretto {
 		}
 		return null;
 	}
+	/**
+	 * Dato un {@link Voto}, determina se esiste già un voto con uguale corso ed uguale punteggio
+	 * @param v
+	 * @return {@code true} se ha trovato un corso con punteggio uguale, 
+	 * 		   {@code false} se non ha trovato il corso, oppure l'ha trovato con voto diverso
+	 */
+	
+	public boolean esisteGiaVoto (Voto v) {
+		Voto trovato= this.cercaEsame(v.getCorso());
+		if(trovato==null)
+			return false;
+		if(trovato.getPunti()==v.getPunti())
+			return true;
+		else
+			return false;
+	}
 	
 
 }
