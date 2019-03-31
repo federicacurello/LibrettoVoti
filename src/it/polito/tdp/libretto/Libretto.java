80d65941a1573f3ba09ea40b486cd0832cae60f8
federica.curello <feffy22@live.it>
2019-03-31 22:14:35 +0200

    punti 5 e 6

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -16,10 +16,17 @@ public class Libretto {
 	/**
 	 * Aggiunge un nuovo voto al libretto
 	 * @param v il {@link Voto} da aggiungere
+	 * @return {@code true} nel caso normale, 
+	 * 			{@code false} se non è riuscito ad aggiungere il voto
 	 */
-	public void add(Voto v) {
+	public boolean add(Voto v) {
+		if(!this.esisteGiaVoto(v)&& !this.votoConflitto(v)) {
 		voti.add(v);
-	}
+		return true;
+	} else {
+		return false;
+		
+	}}
 	/**
 	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
 	 * @param punti punteggio da ricerca
@@ -65,6 +72,24 @@ public class Libretto {
 			return (v.getPunti()== this.voti.get(pos).getPunti() );
 		
 	}
+	/** 
+	 * Mi dice se il {@link Voto} {@code v} è in conflitto con uno dei voti
+	 * esistenti. Se il voto non esiste, non c'è conflitto. Se esiste ed ha
+	 * punteggio diverso, c'è conflitto.
+	 * @param v
+	 * @return {@code true} se il voto esiste ed ha un punteggio diverso
+	 * 			{@code false} se il voto non esiste oppure esiste ma ha lo stesso punteggio
+	 */
+	public boolean votoConflitto(Voto v) {
+		int pos= this.voti.indexOf(v);
+		if(pos==-1)
+			return false;
+		else 
+			return (v.getPunti()!= this.voti.get(pos).getPunti() );
+	}
 	
+	public String toString() {
+		return this.voti.toString();
+	}
 
 }
