db8c82ca51a70449db15e9b18b9814c3186b9966
User <User@192.168.178.20>
2019-03-31 21:48:34 +0200

    concluso punto 4

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -43,12 +43,12 @@ public class Libretto {
 	 */
 	
 	public Voto cercaEsame(String nomeEsame) {
-		for(Voto v: this.voti) {
-			if(v.getCorso().equals(nomeEsame)) {
-				return v;
-			}
-		}
-		return null;
+		Voto voto= new Voto(0, nomeEsame, null);
+		int pos=this.voti.indexOf(voto);
+		if(pos==-1) {
+			return null;}
+		else 
+			return this.voti.get(pos);
 	}
 	/**
 	 * Dato un {@link Voto}, determina se esiste già un voto con uguale corso ed uguale punteggio
@@ -58,13 +58,12 @@ public class Libretto {
 	 */
 	
 	public boolean esisteGiaVoto (Voto v) {
-		Voto trovato= this.cercaEsame(v.getCorso());
-		if(trovato==null)
+		int pos= this.voti.indexOf(v);
+		if(pos==-1)
 			return false;
-		if(trovato.getPunti()==v.getPunti())
-			return true;
-		else
-			return false;
+		else 
+			return (v.getPunti()== this.voti.get(pos).getPunti() );
+		
 	}
 	
 
