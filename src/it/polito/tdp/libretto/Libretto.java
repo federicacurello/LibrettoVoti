e107ea52d943867f4d87f3303ba9a8ff48e3d7b1
User <User@192.168.178.20>
2019-03-31 18:14:07 +0200

    completato punto 2

--- src/it/polito/tdp/libretto/Libretto.java
+++ src/it/polito/tdp/libretto/Libretto.java
@@ -20,5 +20,21 @@ public class Libretto {
 	public void add(Voto v) {
 		voti.add(v);
 	}
+	/**
+	 * Seleziona il sottoinsieme di voti che hanno il punteggio specificato
+	 * @param punti punteggio da ricerca
+	 * @return lista di {@link Voto} aventi quel punteggio (eventualmente vuota)
+	 */
+	
+	public List<Voto> cercaVoti(int voto){
+		List<Voto> result= new ArrayList<Voto>();
+		
+		for(Voto v: this.voti) {
+			if(v.getPunti()==voto) {
+				result.add(v);
+			}
+		}
+		return result;
+	}
 
 }
--- src/it/polito/tdp/libretto/Voto.java
+++ src/it/polito/tdp/libretto/Voto.java
@@ -3,18 +3,18 @@ package it.polito.tdp.libretto;
 import java.time.LocalDate;
 
 public class Voto {
-	private int voto;
+	private int punti;
 	private String corso;
 	private LocalDate data;
 	
 	
-	public int getVoto() {
-		return voto;
+	public int getPunti() {
+		return punti;
 	}
 
 
-	public void setVoto(int voto) {
-		this.voto = voto;
+	public void setPunti(int voto) {
+		this.punti = voto;
 	}
 
 
@@ -39,7 +39,7 @@ public class Voto {
 
 
 	public Voto(int voto, String corso, LocalDate data) {
-		this.voto = voto;
+		this.punti = voto;
 		this.corso = corso;
 		this.data = data;
 	}
@@ -47,7 +47,7 @@ public class Voto {
 
 	@Override
 	public String toString() {
-		return String.format("Voto [voto=%s, corso=%s, data=%s]", voto, corso, data);
+		return String.format("Voto [punti=%s, corso=%s, data=%s]", punti, corso, data);
 	}
 	
 	
